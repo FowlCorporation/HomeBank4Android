f8424f890ff5babfad598a6a189a1c409a2c5ff2
Axel BROCHARD <axel.brochard@telecomnancy.net>
2014-12-04 18:33:27 +0100

    [ADD] Model subPackage [ADD] Test file for HomeBank datas in XML

---
+++ src/com/fowlcorp/homebank4android/model/Category.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.fowlcorp.homebank4android.model;
+
+/**
+ * @author Utilisateur
+ *
+ */
+public class Category {
+
+	/**
+	 * 
+	 */
+	public Category() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
