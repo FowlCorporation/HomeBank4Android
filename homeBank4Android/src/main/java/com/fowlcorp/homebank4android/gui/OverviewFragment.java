d5fbdb06bace71501b56d434e2ea2fb172fe7b01
martinschutz78 <martin.schutz@telecomnancy.net>
2015-01-13 20:10:53 +0100

    [ADD] Account global information

--- homeBank4Android/src/main/java/com/fowlcorp/homebank4android/gui/OverviewFragment.java
+++ homeBank4Android/src/main/java/com/fowlcorp/homebank4android/gui/OverviewFragment.java
@@ -21,10 +21,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -37,6 +41,7 @@ import com.fowlcorp.homebank4android.R;
 import com.fowlcorp.homebank4android.model.Account;
 import com.fowlcorp.homebank4android.model.Model;
 import com.fowlcorp.homebank4android.model.Operation;
+import com.fowlcorp.homebank4android.utils.Round;
 
 public class OverviewFragment extends Fragment{
 
@@ -44,6 +49,10 @@ public class OverviewFragment extends Fragment{
 	private List<Operation> operation;
 	private Model model;
 	private ArrayList<DrawerItem> drawerList;
+
+    private double soldeValue;
+    private double futurValue;
+    private double todayValue;
 	
 	private MainActivity activity;
 	
@@ -82,9 +91,17 @@ public class OverviewFragment extends Fragment{
         TextView solde = (TextView) overViewView.findViewById(R.id.overview_card_solde);
         TextView futur = (TextView) overViewView.findViewById(R.id.overview_card_futur);
         TextView today = (TextView) overViewView.findViewById(R.id.overview_card_today);
-        //ImageView icon = (ImageView) overViewView.findViewById(R.id.overview_card_icon);
+        ImageView icon = (ImageView) overViewView.findViewById(R.id.overview_card_icon);
+
+        soldeValue = Round.roundAmount(model.getGrandTotalBank());
+        futurValue = Round.roundAmount(model.getGrandTotalFuture());
+        todayValue = Round.roundAmount(model.getGrandTotalToday());
 
         title.setText(activity.getString(R.string.Overview));
+        solde.setText(colorText(activity.getString(R.string.Balance) + " : ", String.valueOf(soldeValue)));
+        futur.setText(colorText(activity.getString(R.string.Future) + " : ", String.valueOf(futurValue)));
+        today.setText(colorText(activity.getString(R.string.Today) + " : ", String.valueOf(todayValue)));
+        icon.setImageDrawable(null);
 
 		
 		recycler.setHasFixedSize(false);
@@ -102,4 +119,10 @@ public class OverviewFragment extends Fragment{
 		super.onAttach(activity);
 		((MainActivity) activity).onSectionAttached(0);
 	}
+
+    private Spannable colorText(String fieldName, String value) {
+        Spannable span = new SpannableString(fieldName + value);
+        span.setSpan(new ForegroundColorSpan((value.charAt(0) == '-' ? Color.rgb(206, 92, 0) : Color.rgb(78, 154, 54))), fieldName.length(), fieldName.length() + value.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return span;
+    }
 }
