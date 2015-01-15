ee7b28a1a82ded7365a73339e86f125a44373c87
Axel BROCHARD <axel.brochard@telecomnancy.net>
2015-01-15 10:26:05 +0100

    [FIX] Menu behavior. Fixes #18

--- homeBank4Android/src/main/java/com/fowlcorp/homebank4android/DetailedCardActivity.java
+++ homeBank4Android/src/main/java/com/fowlcorp/homebank4android/DetailedCardActivity.java
@@ -19,6 +19,7 @@
 
 package com.fowlcorp.homebank4android;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
@@ -116,18 +117,22 @@ public class DetailedCardActivity extends ActionBarActivity {
 		return true;
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		} else {
-			onBackPressed();
-			return true;
-		}
-		//return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) { //the settings button is selected
+            Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+            startActivity(intent); //start the activity of preferences
+            return true;
+        }
+        if (id == R.id.action_about) { //the settings button is selected
+            Intent intent = new Intent(getApplicationContext(), AboutActivity.class);
+            startActivity(intent); //start the activity of preferences
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
