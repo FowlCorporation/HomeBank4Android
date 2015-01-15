daa6122cc5eab272d5d858cca816b30e5665823a
Axel BROCHARD <axel.brochard@telecomnancy.net>
2015-01-15 15:42:41 +0100

    [FIX] Copyright [ADD] Custom RadioGroup

--- homeBank4Android/src/main/java/com/fowlcorp/homebank4android/DetailedCardActivity.java
+++ homeBank4Android/src/main/java/com/fowlcorp/homebank4android/DetailedCardActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2015 Fowl Corporation
+ * Copyright © 2014-2015 Fowl Corporation
  *
  * This file is part of HomeBank4Android.
  *
@@ -19,89 +19,103 @@
 
 package com.fowlcorp.homebank4android;
 
-import android.annotation.TargetApi;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
-import android.transition.Explode;
-import android.transition.Fade;
-import android.transition.Transition;
-import android.transition.TransitionInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.fowlcorp.homebank4android.model.PayMode;
 
 public class DetailedCardActivity extends ActionBarActivity {
-    private TextView balance;
 
+    private TextView balance;
+    private RadioGroup detailedCardStateGroup;
 
     @Override
-	protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         this.getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
-		super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
 
-		Bundle bdl = this.getIntent().getExtras();
+        Bundle bdl = this.getIntent().getExtras();
 
-		setContentView(R.layout.activity_detailed_card);
+        setContentView(R.layout.activity_detailed_card);
        /* TransitionInflater transitionInflater = TransitionInflater.from(this);
         final Transition transition = transitionInflater.inflateTransition(R.transition.cardview_fade);
 
         this.getWindow().setExitTransition(transition);*/
 
-		Toolbar toolbar = (Toolbar) findViewById(R.id.detailed_toolbar);
-		
-		setSupportActionBar(toolbar); 
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		
-		//Get ids
+        Toolbar toolbar = (Toolbar) findViewById(R.id.detailed_toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Get ids
+        LinearLayout layout;
         EditText date;
         AutoCompleteTextView category;
         AutoCompleteTextView payee;
         EditText amount;
         EditText wording;
         EditText info;
+        RadioButton none, reconciled, remind, cleared;
 
+        layout = (LinearLayout) findViewById(R.id.detailedCardLayout_root);
         date = (EditText) findViewById(R.id.detailedCardDate);
         category = (AutoCompleteTextView) findViewById(R.id.detailedCardCategory);
         payee = (AutoCompleteTextView) findViewById(R.id.detailedCardPayee);
         amount = (EditText) findViewById(R.id.detailedCardAmount);
         info = (EditText) findViewById(R.id.detailedCardInfo);
         wording = (EditText) findViewById(R.id.detailedCardWording);
+        none = (RadioButton) findViewById(R.id.detailedCardState_none);
+        reconciled = (RadioButton) findViewById(R.id.detailedCardState_reconciled);
+        remind = (RadioButton) findViewById(R.id.detailedCardState_remind);
+        cleared = (RadioButton) findViewById(R.id.detailedCardState_cleared);
 
         ImageView image = (ImageView) findViewById(R.id.detailedCardIcon);
-		
-		try {
-			date.setText(bdl.getString("Date"), TextView.BufferType.SPANNABLE);
-		} catch (Exception e) {
-		}
-		try {
-			category.setText(bdl.getString("Category"));
-		} catch (Exception e) {
-		}
-		try {
-			payee.setText(bdl.getString("Payee"));
-		} catch (Exception e) {
-		}
-		try {
-			wording.setText(bdl.getString("Wording"));
-		} catch (Exception e) {
-		}
+
+        try {
+            date.setText(bdl.getString("Date"), TextView.BufferType.SPANNABLE);
+        } catch (Exception e) {
+        }
+        try {
+            category.setText(bdl.getString("Category"));
+        } catch (Exception e) {
+        }
+        try {
+            payee.setText(bdl.getString("Payee"));
+        } catch (Exception e) {
+        }
+        try {
+            wording.setText(bdl.getString("Wording"));
+        } catch (Exception e) {
+        }
         try {
             info.setText(bdl.getString("Info"));
         } catch (Exception e) {
         }
-		try {
-			amount.setText(bdl.getString("Amount"));
-		} catch (Exception e) {
-		}
+        try {
+            amount.setText(bdl.getString("Amount"));
+        } catch (Exception e) {
+        }
+        try {
+            detailedCardStateGroup = new RadioGroup(this);
+            detailedCardStateGroup.addView(none);
+            detailedCardStateGroup.addView(reconciled);
+            detailedCardStateGroup.addView(remind);
+            detailedCardStateGroup.addView(cleared);
+            layout.addView(detailedCardStateGroup);
+        } catch (Exception e) {
+        }
         int typeAccount = bdl.getInt("Type", -1);
         try {
             switch (typeAccount) {
@@ -129,14 +143,14 @@ public class DetailedCardActivity extends ActionBarActivity {
             }
         } catch (Exception e) {
         }
-	}
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.detailed_card, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.detailed_card, menu);
+        return true;
+    }
 
 
     @Override
