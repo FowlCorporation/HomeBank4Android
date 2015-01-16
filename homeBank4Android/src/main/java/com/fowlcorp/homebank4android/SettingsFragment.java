9d57dd088095c0dc4b63076882e8a5ed7b1f5ee2
martinschutz78 <martin.schutz@telecomnancy.net>
2015-01-16 09:21:57 +0100

    [ADD] Chooser for file path

--- homeBank4Android/src/main/java/com/fowlcorp/homebank4android/SettingsFragment.java
+++ homeBank4Android/src/main/java/com/fowlcorp/homebank4android/SettingsFragment.java
@@ -19,17 +19,52 @@
 
 package com.fowlcorp.homebank4android;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
 
 public class SettingsFragment extends PreferenceFragment{
+
+    static final int DROP_PATH_OK = 1000;
+    private SharedPreferences sharedPreferences; //preferences of the app
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
  
         addPreferencesFromResource(R.xml.fragment_settings);
+        findPreference("dropPath").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent intent = new Intent(getActivity(),DropBoxFileActivity.class);
+                startActivityForResult(intent, DROP_PATH_OK); //start an activity to select a valide file
+                return false;
+            }
+        });
  
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) { //called when an activity whith result ends
+        if(requestCode == DROP_PATH_OK){
+            if(resultCode == Activity.RESULT_OK){ //if the filechooser end correctly
+                String result = data.getStringExtra("pathResult"); //store the new path in the preferences
+                EditTextPreference dropPath = (EditTextPreference) findPreference("dropPath");
+                //dropPath.setText(result);
+                dropPath.getEditText().getText().clear();
+                dropPath.getEditText().getText().append(result);
+                dropPath.getEditText().invalidate();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
 }
