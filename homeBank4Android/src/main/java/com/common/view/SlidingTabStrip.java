bd09e2517ce09ef437e9be5af921909155bed245
Axel BROCHARD <axel.brochard@telecomnancy.net>
2015-01-17 17:49:52 +0100

    [DEL] Unused styles [ADD] Custom colors link in layouts #34

--- homeBank4Android/src/main/java/com/common/view/SlidingTabStrip.java
+++ homeBank4Android/src/main/java/com/common/view/SlidingTabStrip.java
@@ -28,11 +28,11 @@ import android.widget.LinearLayout;
 
 class SlidingTabStrip extends LinearLayout {
 
-    private static final int DEFAULT_BOTTOM_BORDER_THICKNESS_DIPS = 2;
+    private static final int DEFAULT_BOTTOM_BORDER_THICKNESS_DIPS = 0;
     private static final byte DEFAULT_BOTTOM_BORDER_COLOR_ALPHA = 0x26;
-    private static final int SELECTED_INDICATOR_THICKNESS_DIPS = 8;
+    private static final int SELECTED_INDICATOR_THICKNESS_DIPS = 3;
     //private static final int DEFAULT_SELECTED_INDICATOR_COLOR = 0xFF33B5E5;
-    private static final int DEFAULT_SELECTED_INDICATOR_COLOR = Color.parseColor("@color/colorPrimary_Light"); //0xFFFFFFFF; //TODO SelectedIndicator color
+    private static final int DEFAULT_SELECTED_INDICATOR_COLOR = 0xFFFFFFFF;
 
     private static final int DEFAULT_DIVIDER_THICKNESS_DIPS = 1;
     private static final byte DEFAULT_DIVIDER_COLOR_ALPHA = 0x20;
@@ -74,6 +74,7 @@ class SlidingTabStrip extends LinearLayout {
 
         mDefaultTabColorizer = new SimpleTabColorizer();
         mDefaultTabColorizer.setIndicatorColors(DEFAULT_SELECTED_INDICATOR_COLOR);
+        //context.getTheme().getResources().getColor(com.fowlcorp.homebank4android.R.color.colorSelectedTab_Light));  //TODO SelectedIndicator color from color file
         mDefaultTabColorizer.setDividerColors(setColorAlpha(themeForegroundColor,
                 DEFAULT_DIVIDER_COLOR_ALPHA));
 
